4d4e25640208f8e6be12f128373537cce4891716
oleg <danalov.oleg@gmail.com>
2017-10-23 19:00:22 +0300

    added test for front controller

--- WebApp/src/test/java/com/axway/weather/web/controller/FrontControllerTest.java
+++ WebApp/src/test/java/com/axway/weather/web/controller/FrontControllerTest.java
@@ -1,18 +1,30 @@
 package com.axway.weather.web.controller;
 
+import com.axway.weather.web.service.WeatherService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.Model;
 
 import static org.junit.Assert.*;
 
 public class FrontControllerTest {
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void testGetTablePositive() throws Exception {
+        WeatherService weatherServiceMock = Mockito.mock(WeatherService.class);
+        Model modelMock = Mockito.mock(Model.class);
+        FrontController frontController = new FrontController();
 
+
+        frontController.setWeatherService(weatherServiceMock);
+        frontController.getTable("Sofia", null, null, modelMock);
+
+        Mockito.verify(weatherServiceMock).getForecastByCityName("Sofia");
+        Mockito.verify(modelMock).addAttribute("forecast", null);
+
+        frontController.getTable(null, "1", "2", modelMock);
+        Mockito.verify(weatherServiceMock).getForecastByCoordinates("1", "2");
 
     }
 
